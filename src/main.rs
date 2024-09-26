0f7a7dfc8694a3902962a0c745f4e4ffcb8c7bd8
Harvey Lim <13660218+shzhe02@users.noreply.github.com>
2024-09-26 17:03:59 +0300

    half-implementation complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,199 @@
+use std::{fs::read_to_string, sync::Arc};
+
+use naga::{
+    back::spv::{self, Options},
+    front::wgsl,
+    valid::{Capabilities, ValidationFlags, Validator},
+};
+use vulkano::{
+    buffer::{Buffer, BufferCreateInfo, BufferUsage},
+    command_buffer::{
+        allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage,
+    },
+    descriptor_set::{
+        allocator::StandardDescriptorSetAllocator, PersistentDescriptorSet, WriteDescriptorSet,
+    },
+    device::{
+        physical::PhysicalDeviceType, Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo,
+        QueueFlags,
+    },
+    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
+    memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator},
+    pipeline::{
+        compute::ComputePipelineCreateInfo, layout::PipelineDescriptorSetLayoutCreateInfo,
+        ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout,
+        PipelineShaderStageCreateInfo,
+    },
+    shader::{ShaderModule, ShaderModuleCreateInfo},
+    VulkanLibrary,
+};
+
 fn main() {
-    println!("Hello, world!");
+    // === Turn wgsl shader into spirv with Naga ===
+    let doubler = read_to_string("shaders/doubler.wgsl").expect("Failed to read shader file");
+    let shader = wgsl::parse_str(&doubler).expect("Failed to parse file into shader");
+    let mut validator = Validator::new(ValidationFlags::all(), Capabilities::default());
+    let mod_info = validator.validate(&shader).expect("Invalid shader");
+    let options = Options {
+        lang_version: (1, 6),
+        ..Default::default()
+    };
+    let spv_out =
+        spv::write_vec(&shader, &mod_info, &options, None).expect("Failed to create spir-v shader");
+
+    // Starting vulkano implementation
+    let library = VulkanLibrary::new().unwrap();
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+    let device_ext = DeviceExtensions {
+        khr_storage_buffer_storage_class: true,
+        ..DeviceExtensions::empty()
+    };
+    let (physical_device, queue_family_index) = instance
+        .enumerate_physical_devices()
+        .unwrap()
+        .filter(|device| device.supported_extensions().contains(&device_ext))
+        .filter_map(|device| {
+            device
+                .queue_family_properties()
+                .iter()
+                .position(|q| q.queue_flags.intersects(QueueFlags::COMPUTE))
+                .map(|q| (device, q as u32))
+        })
+        .min_by_key(|(device, _)| match device.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            PhysicalDeviceType::Other => 4,
+            _ => 5,
+        })
+        .unwrap();
+    println!(
+        "Device: {}, (type: {:?})",
+        physical_device.properties().device_name,
+        physical_device.properties().device_type
+    );
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            enabled_extensions: device_ext,
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    let queue = queues.next().unwrap();
+    let module = unsafe {
+        ShaderModule::new(device.clone(), ShaderModuleCreateInfo::new(&spv_out))
+            .expect("Failed to create shader module")
+    };
+    let pipeline = {
+        let cs = module.entry_point("doubler").unwrap();
+        let stage = PipelineShaderStageCreateInfo::new(cs);
+        let layout = PipelineLayout::new(
+            device.clone(),
+            PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+                .into_pipeline_layout_create_info(device.clone())
+                .unwrap(),
+        )
+        .unwrap();
+        let params = ComputePipelineCreateInfo::stage_layout(stage, layout);
+        ComputePipeline::new(device.clone(), None, params).unwrap()
+    };
+
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+    let descriptor_set_allocator = Arc::new(StandardDescriptorSetAllocator::new(
+        device.clone(),
+        Default::default(),
+    ));
+    let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
+        device.clone(),
+        Default::default(),
+    ));
+
+    let data_buffer_a = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        0..500u32,
+    )
+    .unwrap();
+    let data_buffer_b = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        5..505u32,
+    )
+    .unwrap();
+    let output_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        0..500u32,
+    )
+    .unwrap();
+
+    let layout = &pipeline.layout().set_layouts()[0];
+    let set = PersistentDescriptorSet::new(
+        &descriptor_set_allocator,
+        layout.clone(),
+        [
+            WriteDescriptorSet::buffer(0, data_buffer_a),
+            WriteDescriptorSet::buffer(1, data_buffer_b),
+            WriteDescriptorSet::buffer(2, output_buffer),
+        ],
+        [],
+    )
+    .unwrap();
+
+    let mut cb = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue.queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .unwrap();
+    cb.bind_pipeline_compute(pipeline.clone())
+        .unwrap()
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            pipeline.layout().clone(),
+            0,
+            set,
+        )
+        .unwrap();
+    cb.dispatch([500, 1, 1]).unwrap();
+
+    // let cb = cb.end_render_pass()
 }
