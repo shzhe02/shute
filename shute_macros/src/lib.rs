91da9cc9c2f1a121773ef645c32faa6742c0f615
Harvey Lim <13660218+shzhe02@users.noreply.github.com>
2024-09-30 22:42:45 +0300

    completed rough implementation

---
+++ shute_macros/src/lib.rs
@@ -0,0 +1,42 @@
+use std::fs::read_to_string;
+
+use naga::{
+    back::spv::{self, Options},
+    front::wgsl,
+    valid::{Capabilities, ValidationFlags, Validator},
+};
+use quote::quote;
+use syn::{parse_macro_input, LitStr};
+
+extern crate proc_macro;
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn load_wgsl_shader(path: TokenStream) -> TokenStream {
+    let path = parse_macro_input!(path as LitStr);
+    let shader_str = read_to_string(&path.value()).expect("Failed to read shader file");
+    let shader = wgsl::parse_str(&shader_str).expect("Failed to parse file into shader");
+    let mut validator = Validator::new(ValidationFlags::all(), Capabilities::default());
+    let mod_info = validator.validate(&shader).expect("Invalid shader");
+    let options = Options {
+        lang_version: (1, 6),
+        ..Default::default()
+    };
+    let out =
+        spv::write_vec(&shader, &mod_info, &options, None).expect("Failed to create spir-v shader");
+    quote! {
+        vec![ #( #out ), *]
+    }
+    .into()
+}
+
+// pub fn get_devices() {
+//     let instance = Instance::new(
+//         VulkanLibrary::new().unwrap(),
+//         InstanceCreateInfo {
+//             flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+//             ..Default::default()
+//         },
+//     )
+//     .unwrap();
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,217 @@
-use std::fs::read_to_string;
-
-use naga::{
-    back::spv::{self, Options},
-    front::wgsl,
-    valid::{Capabilities, ValidationFlags, Validator},
+pub use shute_macros::load_wgsl_shader;
+use vulkano::{
+    buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage, Subbuffer},
+    command_buffer::{
+        allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage,
+    },
+    descriptor_set::{
+        allocator::StandardDescriptorSetAllocator, PersistentDescriptorSet, WriteDescriptorSet,
+    },
+    device::{
+        physical::PhysicalDeviceType, Device, DeviceCreateInfo, DeviceExtensions, Queue,
+        QueueCreateInfo, QueueFlags,
+    },
+    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
+    memory::allocator::{
+        AllocationCreateInfo, FreeListAllocator, GenericMemoryAllocator, MemoryTypeFilter,
+        StandardMemoryAllocator,
+    },
+    pipeline::{
+        compute::ComputePipelineCreateInfo, layout::PipelineDescriptorSetLayoutCreateInfo,
+        ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout,
+        PipelineShaderStageCreateInfo,
+    },
+    shader::{ShaderModule, ShaderModuleCreateInfo},
+    sync::{self, GpuFuture},
+    VulkanLibrary,
 };
-use quote::quote;
-use syn::{parse_macro_input, LitStr};
 
-extern crate proc_macro;
-use proc_macro::TokenStream;
+use std::sync::Arc;
+use std::{collections::HashMap, ops::Deref};
 
-#[proc_macro]
-pub fn load_wgsl_shader(path: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(path as LitStr);
-    let shader_str = read_to_string(&path.value()).expect("Failed to read shader file");
-    let shader = wgsl::parse_str(&shader_str).expect("Failed to parse file into shader");
-    let mut validator = Validator::new(ValidationFlags::all(), Capabilities::default());
-    let mod_info = validator.validate(&shader).expect("Invalid shader");
-    let options = Options {
-        lang_version: (1, 6),
-        ..Default::default()
-    };
-    let out =
-        spv::write_vec(&shader, &mod_info, &options, None).expect("Failed to create spir-v shader");
-    quote! {
-        vec![ #( #out ), *]
+pub struct ComputeDevice<T> {
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    memory_allocator: Arc<GenericMemoryAllocator<FreeListAllocator>>,
+    descriptor_set_allocator: Arc<StandardDescriptorSetAllocator>,
+    command_buffer_allocator: Arc<StandardCommandBufferAllocator>,
+    data_buffers: HashMap<u32, Subbuffer<[T]>>,
+    pipeline: Arc<ComputePipeline>,
+}
+
+impl<T> ComputeDevice<T> {
+    pub fn autoselect(shader: &Vec<u32>, entry_point: &str) -> Self {
+        let library = VulkanLibrary::new().unwrap();
+        let instance = Instance::new(
+            library,
+            InstanceCreateInfo {
+                flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+                ..Default::default()
+            },
+        )
+        .unwrap();
+        let device_ext = DeviceExtensions {
+            khr_storage_buffer_storage_class: true,
+            ..DeviceExtensions::empty()
+        };
+        let (physical_device, queue_family_index) = instance
+            .enumerate_physical_devices()
+            .unwrap()
+            .filter(|device| device.supported_extensions().contains(&device_ext))
+            .filter_map(|device| {
+                device
+                    .queue_family_properties()
+                    .iter()
+                    .position(|q| q.queue_flags.intersects(QueueFlags::COMPUTE))
+                    .map(|q| (device, q as u32))
+            })
+            .min_by_key(|(device, _)| match device.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5,
+            })
+            .unwrap();
+
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                enabled_extensions: device_ext,
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        let queue = queues.next().unwrap();
+
+        // === Creating allocators ===
+
+        let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+        let descriptor_set_allocator = Arc::new(StandardDescriptorSetAllocator::new(
+            device.clone(),
+            Default::default(),
+        ));
+        let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
+            device.clone(),
+            Default::default(),
+        ));
+
+        // Initializing shader module
+
+        let shader_module = unsafe {
+            ShaderModule::new(device.clone(), ShaderModuleCreateInfo::new(shader)).unwrap()
+        };
+
+        let pipeline = {
+            let cs = shader_module.entry_point(entry_point).unwrap();
+            let stage = PipelineShaderStageCreateInfo::new(cs);
+            let layout = PipelineLayout::new(
+                device.clone(),
+                PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+                    .into_pipeline_layout_create_info(device.clone())
+                    .unwrap(),
+            )
+            .unwrap();
+            let params = ComputePipelineCreateInfo::stage_layout(stage, layout);
+            ComputePipeline::new(device.clone(), None, params).unwrap()
+        };
+
+        Self {
+            device,
+            queue,
+            memory_allocator,
+            descriptor_set_allocator,
+            command_buffer_allocator,
+            data_buffers: HashMap::new(),
+            pipeline,
+        }
     }
-    .into()
+    pub fn get_device(&self) -> Arc<Device> {
+        self.device.clone()
+    }
+    pub fn get_queue(&self) -> Arc<Queue> {
+        self.queue.clone()
+    }
+    pub fn execute(&self) {
+        let layout = &self.pipeline.layout().set_layouts()[0];
+        let set = PersistentDescriptorSet::new(
+            &self.descriptor_set_allocator,
+            layout.clone(),
+            self.data_buffers
+                .clone()
+                .into_iter()
+                .map(|(binding, subbuffer)| WriteDescriptorSet::buffer(binding, subbuffer.clone())),
+            [],
+        )
+        .unwrap();
+
+        let mut cb = AutoCommandBufferBuilder::primary(
+            &self.command_buffer_allocator,
+            self.queue.queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+        cb.bind_pipeline_compute(self.pipeline.clone())
+            .unwrap()
+            .bind_descriptor_sets(
+                PipelineBindPoint::Compute,
+                self.pipeline.layout().clone(),
+                0,
+                set,
+            )
+            .unwrap();
+        cb.dispatch([500, 1, 1]).unwrap();
+
+        let cb = cb.build().unwrap();
+
+        let future = sync::now(self.device.clone())
+            .then_execute(self.queue.clone(), cb)
+            .unwrap()
+            .then_signal_fence_and_flush()
+            .unwrap();
+        future.wait(None).unwrap();
+    }
+}
+
+impl<T: BufferContents> ComputeDevice<T> {
+    pub fn add_buffer<I>(&mut self, binding: u32, iter: I)
+    where
+        I: IntoIterator<Item = T>,
+        I::IntoIter: ExactSizeIterator,
+    {
+        self.data_buffers.insert(
+            binding,
+            Buffer::from_iter(
+                self.memory_allocator.clone(),
+                BufferCreateInfo {
+                    usage: BufferUsage::STORAGE_BUFFER,
+                    ..Default::default()
+                },
+                AllocationCreateInfo {
+                    memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                        | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+                    ..Default::default()
+                },
+                iter,
+            )
+            .unwrap(),
+        );
+    }
+}
+impl<T: BufferContents + Clone> ComputeDevice<T> {
+    pub fn read_buffer(&self, buffer_binding: u32) -> Vec<T> {
+        self.data_buffers
+            .get(&buffer_binding)
+            .unwrap()
+            .read()
+            .unwrap()
+            .deref()
+            .to_vec()
+    }
 }
