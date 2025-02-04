e0465ceddf422b91d07abfcf4082de03bc73a9c1
Harvey Lim <13660218+shzhe02@users.noreply.github.com>
2025-02-04 10:35:16 +0200

    fixed type typo

--- examples/shortcut_v0/main.rs
+++ examples/shortcut_v0/main.rs
@@ -30,7 +30,7 @@ fn compute(data: &mut Vec<u32>, dim: u32) {
             output: true,
             read_only: false,
         },
-        shute::BufferInit::<f32>::WithSize(input_buffer.size()),
+        shute::BufferInit::<u32>::WithSize(input_buffer.size()),
     );
     let mut dim_buffer = device.create_buffer(
         Some("dim"),
