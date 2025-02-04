a0e670693bdd3bc0c5f4252fbfd3f8dae420f04d
Harvey Lim <13660218+shzhe02@users.noreply.github.com>
2025-02-04 15:24:43 +0200

    adde benchmarks for multiple consecutive data transfers, also optimized benchmarks

--- benches/benchmarks/io.rs
+++ benches/benchmarks/io.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use criterion::{criterion_group, BenchmarkId, Criterion};
+use criterion::{criterion_group, Criterion};
 use rand::{thread_rng, Rng};
 use shute::{Buffer, BufferInit, BufferType, Device, Instance, LimitType};
 use wgpu::PowerPreference;
@@ -10,26 +10,26 @@ fn generate_data(size: usize) -> Vec<u32> {
     (0..size).map(|_| rng.gen()).collect()
 }
 
-fn initialize_gpu_buffer(device: &Device, data: &Vec<u32>) {
-    device.create_buffer(
+fn initialize_gpu_buffer<'a>(device: &'a Device, data: &'a Vec<u32>) -> Buffer<'a> {
+    let buffer = device.create_buffer(
         Some("test_send"),
         BufferType::StorageBuffer {
-            output: false,
+            output: true,
             read_only: true,
         },
         BufferInit::WithData(data),
     );
     device.queue().submit([]);
-    // buffer.send_data_to_device(device);
     device.block_until_complete();
+    buffer
 }
 
-fn pull_data_from_buffer(device: &Device, buffer: &mut Buffer, output: &mut Vec<u32>) {
-    pollster::block_on(buffer.fetch_data_from_device(device, output));
+fn pull_data_from_buffer(buffer: &mut Buffer, output: &mut Vec<u32>) {
+    pollster::block_on(buffer.fetch_data_from_device(output));
 }
 
-fn benchmark_io(c: &mut Criterion) {
-    let mut group = c.benchmark_group("CPU-GPU IO");
+fn io_oneshot(c: &mut Criterion) {
+    let mut group = c.benchmark_group("CPU-GPU Data Transfer (One-Shot)");
     group
         .sample_size(10)
         .noise_threshold(0.15)
@@ -43,26 +43,59 @@ fn benchmark_io(c: &mut Criterion) {
     // Benchmarks for sending data from CPU to GPU
 
     let size = device.limits().max_buffer_size as usize / size_of::<u32>();
+    dbg!(size);
+    let mut buffer: Option<Buffer> = None;
+    let data = generate_data(size);
 
-    group.bench_with_input(
-        BenchmarkId::new("Buffer Initialization", size),
-        &generate_data(size),
-        |b, i| b.iter(|| initialize_gpu_buffer(&device, i)),
-    );
+    group.bench_function("Buffer Initialization", |b| {
+        b.iter(|| {
+            buffer = Some(initialize_gpu_buffer(&device, &data));
+        })
+    });
 
     // Benchmarks for pulling data from GPU to CPU
-    let mut buffer = device.create_buffer(
-        Some("test_pull"),
-        BufferType::StorageBuffer {
-            output: true,
-            read_only: true,
-        },
-        BufferInit::WithSize::<u32>(size as u32),
-    );
     let mut output = vec![0; size];
     group.bench_function("Reading Data from GPU Buffer", |b| {
-        b.iter(|| pull_data_from_buffer(&device, &mut buffer, &mut output))
+        b.iter(|| pull_data_from_buffer(buffer.as_mut().unwrap(), &mut output))
+    });
+}
+fn io_tenshot(c: &mut Criterion) {
+    let mut group = c.benchmark_group("CPU-GPU Data Transfer (10-Shot)");
+    group
+        .sample_size(10)
+        .noise_threshold(0.15)
+        .measurement_time(Duration::from_secs(10));
+    let instance = Instance::new();
+    let device = pollster::block_on(
+        instance.autoselect(PowerPreference::HighPerformance, LimitType::Downlevel),
+    )
+    .unwrap();
+
+    // Benchmarks for sending data from CPU to GPU
+
+    let n = 10;
+
+    let size = 1000;
+    let mut buffers = vec![];
+    let data = generate_data(size);
+
+    group.bench_function("Buffer Initialization", |b| {
+        b.iter(|| {
+            (0..n).for_each(|_| {
+                buffers.push(initialize_gpu_buffer(&device, &data));
+            });
+        });
+    });
+
+    // Benchmarks for pulling data from GPU to CPU
+    let mut output = vec![0; size];
+    group.bench_function("Reading Data from GPU Buffer", |b| {
+        b.iter(|| {
+            buffers
+                .iter_mut()
+                .for_each(|buffer| pull_data_from_buffer(buffer, &mut output))
+        });
     });
 }
 
-criterion_group!(io, benchmark_io);
+criterion_group!(io, io_oneshot, io_tenshot);
